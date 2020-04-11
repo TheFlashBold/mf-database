29138daa33faba1bac72372356e51de100916ab9
tycho <tycho.holzer@gmail.com>
2020-04-11 19:24:50 +0200

    added generic api

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const DatabaseModule = require('./lib/DatabaseModule');
 
 module.exports = {
-    module: DatabaseModule
+    module: DatabaseModule,
+    data: {
+        apis: [
+            require('./apis/v0.1')
+        ]
+    }
 };
